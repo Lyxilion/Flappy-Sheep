6dbbed8443c33ed8535b5fee538d5f2f446a154f
Lyxilion <boucher.rubens.974@gmail.com>
2016-04-21 18:28:37 +0200

    1.2.0

---
+++ src/GameOver.js
@@ -0,0 +1,40 @@
+
+BasicGame.GameOver = function (game) {
+	 
+};
+
+BasicGame.GameOver.prototype = {
+
+    create: function () {
+		this.game.add.sprite(0,0,"background");
+		
+		var title = this.game.add.sprite(500,-100,'title');
+		title.anchor.setTo(0.5,0.5);
+		this.game.add.tween(title).to({ y: 100 },2000, Phaser.Easing.Elastic.InOut, true, 0);	
+		var scoreText = this.game.add.text(500, 150, "" , { font: '30px Courrier New', fill: '#fff' });
+		scoreText.setText(count);
+		
+		var playButton = this.game.add.button(500,500,"play",this.play,this,1,0)
+		playButton.anchor.setTo(0.5,0.5);
+		
+		this.game.add.tween(playButton).to({ y: 275 },2000, Phaser.Easing.Elastic.InOut, true, 0);
+		this.score();
+    },
+	
+	play : function () {
+		this.game.state.start("Game");
+	},
+	
+	score : function() {
+		
+		var sSaveUserName  = GJAPI.sUserName;
+        var sSaveUserToken = GJAPI.sUserToken;
+		if (GJAPI.sUserName) {
+			GJAPI.UserLoginManual(sSaveUserName, sSaveUserToken);
+			GJAPI.ScoreAdd(80993, count, count + " points");
+		}
+		else {
+			GJAPI.ScoreAddGuest(80993, count, count + " points", "Guest");
+		}		
+	}
+};
